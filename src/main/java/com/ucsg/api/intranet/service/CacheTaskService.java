acb9e9fe0d82ae83d8e3b67c108de8abc1751359
JhonnyTomala <plataformas.computo@cu.ucsg.edu.ec>
2026-07-01 11:17:55 -0500

    Reapply "change"
    This reverts commit cd0cd73f8986b9b06f0dd083c9c687c9a1649843.

--- src/main/java/com/ucsg/api/intranet/service/CacheTaskService.java
+++ src/main/java/com/ucsg/api/intranet/service/CacheTaskService.java
@@ -8,12 +8,27 @@ import org.slf4j.LoggerFactory;
 
 @Service
 public class CacheTaskService {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(CacheTaskService.class);
-    
-    @Scheduled(fixedRate = 60 * 60 * 1000) // Cada hora
-    @CacheEvict(value = {"cumpleanioMes", "contactos", "cacheJerarquiaUnidades"}, allEntries = true)
-    public void clearCache() {
-        logger.info("Cache cleared");
+
+    // Noticias: refresco cada 30 minutos
+    @Scheduled(fixedRate = 30 * 60 * 1000)
+    @CacheEvict(value = "noticias", allEntries = true)
+    public void clearNoticias() {
+        logger.info("Cache de noticias limpiado");
+    }
+
+    // Directorio de contactos: refresco cada 4 horas
+    @Scheduled(fixedRate = 4 * 60 * 60 * 1000)
+    @CacheEvict(value = {"contactos", "cacheJerarquiaUnidades"}, allEntries = true)
+    public void clearDirectorio() {
+        logger.info("Cache de directorio limpiado");
+    }
+
+    // Cumpleaños: refresco cada hora
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    @CacheEvict(value = "cumpleanioMes", allEntries = true)
+    public void clearCumpleanios() {
+        logger.info("Cache de cumpleanios limpiado");
     }
 }
