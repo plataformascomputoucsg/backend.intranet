cd0cd73f8986b9b06f0dd083c9c687c9a1649843
JhonnyTomala <plataformas.computo@cu.ucsg.edu.ec>
2026-07-01 10:24:58 -0500

    Revert "change"
    This reverts commit 8e0a7841dca6be620a5bf86864ee11777c1e0206.

--- src/main/java/com/ucsg/api/intranet/repository/ComunicacionesRepository.java
+++ src/main/java/com/ucsg/api/intranet/repository/ComunicacionesRepository.java
@@ -1,6 +1,5 @@
 package com.ucsg.api.intranet.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,7 +11,6 @@ import java.util.List;
 @Repository
 public interface ComunicacionesRepository extends JpaRepository<Comunicaciones, Integer> {
 
-    @Cacheable(value = "noticias", key = "#tipoEvento + '-' + #seccion + '-' + #tipSitio")
     @Query(value = "SELECT CODIGO, DESCRIPCION, FEC_INICIO, DIR_IMAGEN, TITULO, CATEGORIA, HIGHLIGHT, DETALLE_DESC, TIPO_EVENTO, SECCION, INDICE, TIP_SITIO, DESTACADO, SUBSECCION, UBICACION, DESC_ORGANIZA " +
                    "FROM CCE_VW_COMUNICACIONES " +
                    "WHERE TIPO_EVENTO = ?1 AND SECCION = ?2 AND TIP_SITIO = ?3 AND ESTADO = 'A' ORDER BY INDICE", nativeQuery = true)
--- src/main/java/com/ucsg/api/intranet/service/CacheTaskService.java
+++ src/main/java/com/ucsg/api/intranet/service/CacheTaskService.java
@@ -8,27 +8,12 @@ import org.slf4j.LoggerFactory;
 
 @Service
 public class CacheTaskService {
-
+    
     private static final Logger logger = LoggerFactory.getLogger(CacheTaskService.class);
-
-    // Noticias: refresco cada 30 minutos
-    @Scheduled(fixedRate = 30 * 60 * 1000)
-    @CacheEvict(value = "noticias", allEntries = true)
-    public void clearNoticias() {
-        logger.info("Cache de noticias limpiado");
-    }
-
-    // Directorio de contactos: refresco cada 4 horas
-    @Scheduled(fixedRate = 4 * 60 * 60 * 1000)
-    @CacheEvict(value = {"contactos", "cacheJerarquiaUnidades"}, allEntries = true)
-    public void clearDirectorio() {
-        logger.info("Cache de directorio limpiado");
-    }
-
-    // Cumpleaños: refresco cada hora
-    @Scheduled(fixedRate = 60 * 60 * 1000)
-    @CacheEvict(value = "cumpleanioMes", allEntries = true)
-    public void clearCumpleanios() {
-        logger.info("Cache de cumpleanios limpiado");
+    
+    @Scheduled(fixedRate = 60 * 60 * 1000) // Cada hora
+    @CacheEvict(value = {"cumpleanioMes", "contactos", "cacheJerarquiaUnidades"}, allEntries = true)
+    public void clearCache() {
+        logger.info("Cache cleared");
     }
 }
